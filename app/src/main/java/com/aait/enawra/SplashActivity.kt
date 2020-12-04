c3394278eed921c9a9ef896c3e6fddad9338f3e4
yam <yamlakyam@gmail.com>
2020-12-04 15:30:38 +0300

    added splash screen

---
+++ app/src/main/java/com/aait/enawra/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.aait.enawra
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        val handler: Handler = Handler()
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                val intent= Intent(this@SplashActivity, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        },3000)
+    }
+}
